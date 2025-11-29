a5026bd8521721f2d3de3f9f2b6719209a25e558
Liybomyr Voskalo <voskalo.pn@ucu.edu.ua>
2025-11-30 01:12:59 +0200

    Дописав доки до функцій і коменти

--- data.py
+++ data.py
@@ -1,5 +1,5 @@
 '''
-Create data from file
+Functions to get data from file
 '''
 
 
@@ -11,15 +11,20 @@ Create data from file
 def read_data(filepath: str) -> list:
     '''
     function to read data from file with certain order
+    Приймає назву файлу і повертає списоск списків(секцій) з файлу для подальшої обробки
+
+    Args:
+        filepayth (str): path to file
+    Returns:
+        list: list of sections from file
+
     '''
     if not isinstance(filepath, str):
         raise ValueError('Неправильний ввід даних у функцію !')
 
-    # with open(filepath, 'r', encoding='utf-8') as file:
-    #     all_txt = [el.strip() for el in file.readlines() if el.strip()]
-
     with open(filepath, 'r', encoding='utf-8') as file:
         all_txt = iter(el.strip() for el in file.readlines() if el.strip())
+        # вся дата через генератор для оптиміщації
 
     whole_sorted = []
     section = []
@@ -45,6 +50,17 @@ def read_data(filepath: str) -> list:
 def create_comp_dict(content: list) -> dict:
     '''
     creates dict of dict with несумісність of components
+
+    Args:
+        content (list): повний ліст інфи від файлу(всі секції)
+
+    Returns:
+        dict: словник з сумісністю елементів(True - сумісні, False - не сумісні)(компоненти - ключі зовніщнього,
+                                     наступний компонент - той який хочемо порівняти, і значення буде True/False)
+
+    Приклад:
+        dict[Компонент1][Компонент1]
+        True
     '''
     if not isinstance(content, list):
         raise ValueError('Некоректний ввід в функцію, перевірте, що ви ввели (create_dict)')
@@ -55,6 +71,7 @@ def create_comp_dict(content: list) -> dict:
         comp_dict = {}
 
         for el in content[2][1:]:
+            # словник для всіх компонент
             comp_dict[el] = dict()
 
             for el1 in content[2][1:]:
@@ -64,6 +81,7 @@ def create_comp_dict(content: list) -> dict:
         for key, dic in comp_dict.items():
 
             for el in content[3][1:]:
+                # ті які не сумісні False
                 comp1, comp2 = remake(el)
                 if comp1 == key:
                     dic[comp2] = False
@@ -85,6 +103,15 @@ def create_comp_dict(content: list) -> dict:
 def create_packets(content: list) -> dict:
     '''
     creates dict with necessary comps
+
+    Args:
+        content (list): повний ліст інфи від файлу(всі секції)
+
+    Returns:
+        dict: словник з пакетами(ключ - назва пакету, значення - список компонентів в пакеті)
+
+    Приклад:
+        dict[Пакет1] = [Компонент1, Компонент2, ...]
     '''
     if not isinstance(content, list):
         raise ValueError('Направильний ввід create_packets()')
@@ -93,7 +120,6 @@ def create_packets(content: list) -> dict:
 
     try:
         packets = {}
-        # Пакети тепер у content[5]
         itter = iter(content[5][1:])
 
         for el in itter:
@@ -115,6 +141,13 @@ def create_packets(content: list) -> dict:
 def packet_remake(line: str) -> list:
     '''
     helping function that divides line into parts for packets for better handling
+    Допоміжна функція, яка ділить рядок на частини для пакетів для кращої обробки
+
+    Args:
+        line (str): рядок з пакетом(назва пакету: компонент1, компонент2,...)
+
+    Returns:
+        list: список з назвою пакету і списком компонентів в ньому
     '''
     if not isinstance(line, str):
         raise ValueError('Ви погано ввели дані в функцію packet_remake')
@@ -141,6 +174,13 @@ def packet_remake(line: str) -> list:
 def remake(line: str) -> list:
     '''
     divides line into parts
+    Допоміжна функція, яка ділить рядок на частини за словом "і"
+
+    Args:
+        line (str): рядок з двома компонентами(Компонент1 і Компонент2)
+
+    Returns:
+        list: список з двох компонентів
     '''
     if not isinstance(line, str):
         raise ValueError('Ви погано ввели дані в функцію remake')
