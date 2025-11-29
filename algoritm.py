ec52d49862efc0a83abd52a4901770d44e7fc77e
Zabulskyi Mykola <71492385+MasterDay3@users.noreply.github.com>
2025-11-29 13:29:50 +0200

    delete all

--- algoritm.py
+++ algoritm.py
@@ -1,14 +1,1 @@
-print('hello world')
-print('hello world')
-print('lutuy gas')
-
-a = 10 
-b = a + 2
-print(b)
-print()
-
-def potuzhnist():
-  return
-print('pusshh')
-print('q123')
-print('1')
+'''ALGS'''
