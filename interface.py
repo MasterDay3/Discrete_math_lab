d0dd5c289e4c8acbb96577816e7dfefc63fb4f68
pavlikravlikk <krailo.pn@ucu.edu.ua>
2025-12-04 01:49:18 +0200

    Трохи дописав інтерфейс

--- interface.py
+++ interface.py
@@ -15,9 +15,16 @@ FILENAME = "data_test.txt"
 all_txt1 = read_data(FILENAME)
 comp_dict = create_comp_dict(all_txt1)
 
+def show_header_name(all_text: list):
+    for i in all_text:
+        if '===== Назва =====' in i:
+            st.title(i[1])
+
+
 def show_components_checkboxes(components: dict, column_count: int = 4):
     """
-    Виводить всі компоненти на екран як чекбокси і повертає обрані.
+    Виводить всі компоненти на екран з вибраною
+    довжиною рядків(column_count) як чекбокси і повертає обрані.
     """
     st.subheader("Виберіть компоненти для збірки замовлення:")
 
@@ -28,6 +35,22 @@ def show_components_checkboxes(components: dict, column_count: int = 4):
         column[i % column_count].checkbox(component)
         i += 1
 
+def main():
+    """
+    Основна функція виводу інтерфейсу
+    """
+    show_header_name(all_txt1)
+    show_components_checkboxes(comp_dict)
+    st.title('Збірка користувацьких замовлень.')
+    st.markdown("<span style='color: blue;'>Визначимо, чи можливо зібрати Ваше \
+    кастомне замовлення на основі залежностей між компонентами.</span>", unsafe_allow_html=True)
+
+    FILENAME = "data_test.txt"
+    all_txt1 = read_data(FILENAME)
+    comp_dict = create_comp_dict(all_txt1)
+
+show_header_name(all_txt1)
 show_components_checkboxes(comp_dict)
+
 
 
