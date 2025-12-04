f206428f566dcb65bc0a0a25e7268206062efe95
yuliiayakymiv <yuliia.yakymiv08@gmail.com>
2025-12-04 23:01:53 +0200

    Update interface.py
    підкорегувала main, зробила останню кнопку

--- interface.py
+++ interface.py
@@ -1,25 +1,45 @@
 """Interface"""
 import streamlit as st
+from data import create_comp_dict, read_data
+from algoritm import check_request, add_component
+
 # import pandas as pd
 # import numpy as np
 
-"""Імпортуєм інші модулі"""
-from data import create_comp_dict, read_data
-from algoritm import check_request
-
-st.title('Збірка користувацьких замовлень.')
-st.markdown("<span style='color: blue;'>Визначимо, чи можливо зібрати Ваше \
-кастомне замовлення на основі залежностей між компонентами.</span>", unsafe_allow_html=True)
-
 FILENAME = "data_test.txt"
-all_txt1 = read_data(FILENAME)
-comp_dict = create_comp_dict(all_txt1)
+
+def introduction():
+    st.title('Збірка користувацьких замовлень.')
+    name = st.text_input("Введіть своє ім'я:")
+    if name:
+        st.write(f"<span style='color: blue;'>Вітаємо, {name}!</span>", unsafe_allow_html=True)
+        return name
+    st.markdown("<span style='color: blue;'>Визначимо, чи можливо зібрати Ваше \
+    кастомне замовлення на основі залежностей між компонентами.</span>", unsafe_allow_html=True)
+    
+
+def final_button(name):
+    if st.button("Зібрати замовлення"):
+        if add_component():
+            st.markdown(
+                f"<div style='padding:50px; font-size:32px; color:green; text-align:center;'>"
+                f"Вітання {name if name else 'користувач'}! Все добре, замовлення прийнято!"
+                "</div>",
+                unsafe_allow_html=True
+            )
+        else:
+            st.markdown(
+                f"<div style='padding:50px; font-size:32px; color:red; text-align:center;'>"
+                f"Шановний {name if name else 'користувач'}, на жаль, суміжність між компонентами відсутня. Замовлення не виконане."
+            )
+        
+# all_txt1 = read_data(FILENAME)
+# comp_dict = create_comp_dict(all_txt1)
 
 def show_header_name(all_text: list):
     for i in all_text:
         if '===== Назва =====' in i:
             st.title(i[1])
-
 
 def show_components_checkboxes(components: dict, column_count: int = 4):
     """
@@ -39,18 +59,13 @@ def main():
     """
     Основна функція виводу інтерфейсу
     """
-    show_header_name(all_txt1)
-    show_components_checkboxes(comp_dict)
-    st.title('Збірка користувацьких замовлень.')
-    st.markdown("<span style='color: blue;'>Визначимо, чи можливо зібрати Ваше \
-    кастомне замовлення на основі залежностей між компонентами.</span>", unsafe_allow_html=True)
-
-    FILENAME = "data_test.txt"
     all_txt1 = read_data(FILENAME)
     comp_dict = create_comp_dict(all_txt1)
 
-show_header_name(all_txt1)
-show_components_checkboxes(comp_dict)
+    name = introduction()
+    show_header_name(all_txt1)
+    show_components_checkboxes(comp_dict)
+    final_button(name)
 
-
-
+if __name__ == "__main__":
+    main()
